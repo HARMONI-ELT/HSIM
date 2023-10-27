- Various Minimum Compliant Mode fixes PSF E2E correction HARMONI thermal background normalization Scattered sky ELT reflectivity

hsim/src/modules/create_psf.py
@@ -460,8 +460,9 @@ def define_psf(input_parameters, _jitter, _fov, _psfscale, rotation=None):
 			target_FWHM = tiptop_FWHM*(1. + correction_fwhm)
 			
 			kernel_e2e_corr_sigma = (target_FWHM**2 - tiptop_FWHM**2)**0.5/2.35482
-			
-			kernel_e2e_corr = Gauss2D_instrument(xx, yy)
+			Gauss2D_e2e = lambda x, y: 1.*np.exp(-(x**2 + y**2)/(2.*kernel_e2e_corr_sigma**2))
+
+			kernel_e2e_corr = Gauss2D_e2e(xx, yy)
 			kernel_e2e_corr = kernel_e2e_corr/np.sum(kernel_e2e_corr)
 			user_psf_AO = fftconvolve(user_psf_AO, kernel_e2e_corr, mode="same")
 			peak_psf_AO = np.max(user_psf_AO)

hsim/src/sim_instrument.py
@@ -247,7 +247,6 @@ def sim_instrument(input_parameters, cube, back_emission, transmission, ext_lamb
 	grating = input_parameters["grating"]
 	harmoni.addPart(InstrumentPart("Grating " + grating, TCryoMech, AreaIns, n_mirrors=1, emis_mirror=grating + "_grating.txt", dust_mirror=0))
 	
-	lamb_grid = np.linspace(2, 2.5, 50)
 	HARMONI_transmission, HARMONI_background = harmoni.calcThroughputAndEmission(ext_lambs, input_parameters["exposure_time"], output_file=output_file)
 	
 	if input_parameters["mci"]:
@@ -260,10 +259,10 @@ def sim_instrument(input_parameters, cube, back_emission, transmission, ext_lamb
 		scaling_transmission = 1./np.mean(mci_HARMONI_transmission)*0.26
 		HARMONI_transmission = np.interp(ext_lambs, mci_lamb, mci_HARMONI_transmission*scaling_transmission)
 		
-		scaling_background = 2.031e3/(np.median(HARMONI_background)/input_parameters["exposure_time"])*np.median(HARMONI_transmission)/0.289
-		HARMONI_background = scaling_background*HARMONI_background # scaled background to match specifictaion at 2.2um at 9C
+		#scaling_background = 2.031e3/(np.median(HARMONI_background)/input_parameters["exposure_time"])*np.median(HARMONI_transmission)/0.289
+		scaling_background = 1.0754 # scaled background to match specifictaion at 2.2um at 9C
+		HARMONI_background = scaling_background*HARMONI_background
 		
-		#HARMONI_background = HARMONI_background*0.6341 # scaled background to match specifictaion at 2.2um at 9C
 
 		logging.info("Total MCI HARMONI backround: lambda = {:7.4f} throughput = {:6.3f} emission = {:.3e} ph/um/m2/arcsec2/s".format(np.median(ext_lambs), np.median(HARMONI_transmission), np.median(HARMONI_background)/input_parameters["exposure_time"]))
 

hsim/src/sim_sky.py
@@ -80,13 +80,13 @@ def sky_background(input_parameters, lambs, air_mass, dit, input_spec_res, debug
 	# estimate scattered background
 	bandws = config_data['gratings'][input_parameters["grating"]]
 	mask_range_scatter = (sky_em_lambda > bandws.lmin)*(sky_em_lambda < bandws.lmax)
-	mean_sky = np.mean(sky_em_flux[mask_range_scatter])
+	mean_sky = np.median(sky_em_flux[mask_range_scatter])
 	if input_parameters["mci"]:
 		scattered_sky = mean_sky*0.2
 	else:
 		scattered_sky = mean_sky*input_parameters["scattered_sky"]/100.
 	
-	logging.info("Extra scatter sky = " + str(scattered_sky) + " photons/m2/um/arcsec2")
+	logging.info("Extra scatter sky = " + str(scattered_sky) + " photons/m2/um/arcsec2/s")
 	
 
 	mask_range_output = (sky_em_lambda > lambs[0]*0.99)*(sky_em_lambda < lambs[-1]*1.01)